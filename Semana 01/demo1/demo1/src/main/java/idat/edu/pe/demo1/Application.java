befd7d8a3382bb6fabbdff084997ee8f82ea4557
alexis5999 <alexis5999@gmail.com>
2021-10-07 16:26:23 -0500

    message

---
+++ Semana 01/demo1/demo1/src/main/java/idat/edu/pe/demo1/Application.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
