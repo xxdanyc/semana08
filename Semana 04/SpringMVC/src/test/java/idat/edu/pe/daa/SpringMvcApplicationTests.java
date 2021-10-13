2cc579beef7e19b79dda4a2414a36906b9e9eb10
alexis5999 <alexis5999@gmail.com>
2021-10-13 01:12:25 -0500

    Se sube el avance de la semana 04

---
+++ Semana 04/SpringMVC/src/test/java/idat/edu/pe/daa/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.daa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
