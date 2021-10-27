47b4633e907f60acab357859f0d92368d9842e0a
alexis5999 <alexis5999@gmail.com>
2021-10-26 20:06:17 -0500

    Avance de Repositorios

---
+++ Semana 05/SpringMVCWebApp/src/main/java/idat/edu/pe/daa2/jpa/repositorios/CategoriaRepositorio.java
@@ -0,0 +1,9 @@
+package idat.edu.pe.daa2.jpa.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import idat.edu.pe.daa2.jpa.modelo.Categoria;
+
+public interface CategoriaRepositorio extends CrudRepository<Categoria, Integer> {
+
+}
---
+++ Semana 05/SpringMVCWebApp/src/main/java/idat/edu/pe/daa2/jpa/repositorios/FuncionesRepositorio.java
@@ -0,0 +1,9 @@
+package idat.edu.pe.daa2.jpa.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import idat.edu.pe.daa2.jpa.modelo.Funciones;
+
+public interface FuncionesRepositorio extends CrudRepository<Funciones, Integer> {
+
+}
