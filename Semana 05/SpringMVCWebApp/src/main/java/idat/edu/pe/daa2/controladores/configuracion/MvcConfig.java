f6f4a5c7c29e282a86c4bf81abadd5476ef13d51
alexis5999 <alexis5999@gmail.com>
2021-11-02 22:40:06 -0500

    Actualizando web front end

--- Semana 05/SpringMVCWebApp/src/main/java/idat/edu/pe/daa2/controladores/configuracion/MvcConfig.java
+++ Semana 05/SpringMVCWebApp/src/main/java/idat/edu/pe/daa2/controladores/configuracion/MvcConfig.java
@@ -2,9 +2,10 @@ package idat.edu.pe.daa2.controladores.configuracion;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
 		
--- Semana 05/SpringMVCWebApp/src/main/java/idat/edu/pe/daa2/controladores/configuracion/WebSecurityConfig.java
+++ Semana 05/SpringMVCWebApp/src/main/java/idat/edu/pe/daa2/controladores/configuracion/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package idat.edu.pe.daa2.controladores.configuracion;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +33,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	}
 	
-	
-	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("ADMIN").and()
+		.withUser("jorge").password(encoder.encode("jorge")).roles("LECTOR").and()
+		.withUser("maria").password(encoder.encode("maria")).roles("CREADOR").and()
+		.withUser("elena").password(encoder.encode("elena")).roles("LECTOR","DEPURADOR").and()
+		.withUser("ernesto").password(encoder.encode("ernesto")).roles("EDITOR","LECTOR").and();
+
+	}
+		
 }
